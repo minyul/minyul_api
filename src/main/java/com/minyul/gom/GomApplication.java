589b01cd106cb229863a95dacf332c5842a4dddf
minyul <ggomjae@gmail.com>
2021-12-27 10:03:43 +0900

    init

---
+++ src/main/java/com/minyul/gom/GomApplication.java
@@ -0,0 +1,13 @@
+package com.minyul.gom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomApplication.class, args);
+	}
+
+}
