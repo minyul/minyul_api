8e51d4dc076a3743b24c233d4b3d04e7a59a5629
minyul <ggomjae@gmail.com>
2021-12-31 01:19:22 +0900

    fix : @Builder 패턴 -> 생성자 필요

--- app-api/src/main/java/com/minyul/api/external/ExternalDto.java
+++ app-api/src/main/java/com/minyul/api/external/ExternalDto.java
@@ -1,11 +1,13 @@
 package com.minyul.api.external;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class ExternalDto {
 
