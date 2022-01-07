4cbae0afaf18eb2616586e4ab233fcf84e97b719
minyul <ggomjae@gmail.com>
2022-01-07 22:41:53 +0900

    test : h2 test db - stub -> real

--- module-rds/src/test/java/com/minyul/rds/repository/PostRepositoryTest.java
+++ module-rds/src/test/java/com/minyul/rds/repository/PostRepositoryTest.java
@@ -7,28 +7,69 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
+
+/**
+ * stub으로만 테스트할게 아님. Dao쪽은
+ * @author Minyul
+ */
+//@ExtendWith(MockitoExtension.class)
+//class PostRepositoryTest {
+//
+//	@Mock
+//	PostRepository postRepository;
+//
+//	@DisplayName("피드를 저장하고 저장한 피드를 잘 갖고오는 지 확인")
+//	@Test
+//	void test_save_post() {
+//
+//		// 전제
+//		final Post post = new Post(1L, "title");
+//		given(postRepository.save(post)).willReturn(post);
+//
+//		Post resultPost = postRepository.save(post);
+//
+//		verify(postRepository, times(1)).save(post);
+//		Assertions.assertEquals(post.getId(), resultPost.getId(), 2L);
+//	}
+//}
+
+
+/**
+ *  DateJpaTest - 트랜잭션 있으므로 자동 롤백됨. Good
+ *  By default, tests annotated with {@code @DataJpaTest} are transactional and roll back
+ *  @author : Minyul
+ */
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 class PostRepositoryTest {
 
-	@Mock
+	@Autowired
 	PostRepository postRepository;
 
 	@DisplayName("피드를 저장하고 저장한 피드를 잘 갖고오는 지 확인")
 	@Test
 	void test_save_post() {
 
-		// 전제
-		final Post post = new Post(1L, "title");
-		given(postRepository.save(post)).willReturn(post);
+		// when
+		final Post post = Post.builder()
+						.title("minyul")
+						.build();
 
-		Post resultPost = postRepository.save(post);
+		// given
+		final Post resultPost = postRepository.save(post);
 
-		verify(postRepository, times(1)).save(post);
-		Assertions.assertEquals(post.getId(), resultPost.getId(), 2L);
+		// then
+		assertThat(post.getId()).isEqualTo(resultPost.getId());
+		assertThat(post.getTitle()).isEqualTo(resultPost.getTitle());
 	}
 }
