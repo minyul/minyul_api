325f5720b500f9b18824799f022a6ce79abc0e9c
minyul <ggomjae@gmail.com>
2022-01-09 15:25:09 +0900

    refactor : @Before, @After

--- app-api/src/main/java/com/minyul/api/aop/LogAspect.java
+++ app-api/src/main/java/com/minyul/api/aop/LogAspect.java
@@ -1,8 +1,8 @@
 package com.minyul.api.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -12,8 +12,24 @@ import java.time.LocalDateTime;
 @Component
 public class LogAspect {
 
-	@Around("execution(* com.minyul.api.facade.service.PostService.*(..))")
-	public void testLog() {
+	/**
+	 *  @Around()
+	 *  @AfterThrowing
+	 *  @AfterReturning
+	 *  @Before
+	 *  @After
+	 *  만약, 갖고오는 메소드만 하고 싶다면, PostService.retrieve*(..)
+	 */
+
+	@Before("execution(* com.minyul.api.facade.service.PostService.*(..))")
+	public void beforeLog(JoinPoint joinPoint) {
+		log.info("Method Name : {}", joinPoint.getSignature().getName());
+		log.info("AOP TEST : {}", LocalDateTime.now());
+	}
+
+	@After("execution(* com.minyul.api.facade.service.PostService.*(..))")
+	public void afterLog(JoinPoint joinPoint) {
+		log.info("Method Name : {}", joinPoint.getSignature().getName());
 		log.info("AOP TEST : {}", LocalDateTime.now());
 	}
 }
