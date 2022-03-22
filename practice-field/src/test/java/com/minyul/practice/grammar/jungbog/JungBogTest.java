71413961b4f14fc316599b55859abee45e080408
minyul <ggomjae@gmail.com>
2022-03-22 22:20:43 +0900

    test : map test

--- practice-field/src/test/java/com/minyul/practice/grammar/jungbog/JungBogTest.java
+++ practice-field/src/test/java/com/minyul/practice/grammar/jungbog/JungBogTest.java
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("중복 테스트")
@@ -29,6 +34,12 @@ public class JungBogTest {
 	@DisplayName("객체 중복 Map 테스트")
 	@Test
 	void JungBogServiceTest() {
-		System.out.println(jungBogService);
+		BundleDto bundleDto = new BundleDto(new KeyDto("key_1"), new ValueDto("a"));
+		BundleDto bundleDto1 = new BundleDto(new KeyDto("key_1"), new ValueDto("b"));
+		BundleDto bundleDto2 = new BundleDto(new KeyDto("key_2"), new ValueDto("a"));
+		List<BundleDto> bundleDtos = new ArrayList<>(List.of(bundleDto1, bundleDto2, bundleDto));
+		Map<KeyDto, List<ValueDto>> result = jungBogService.jungBogMethod(bundleDtos);
+
+		assertThat(result.size()).isEqualTo(2);
 	}
 }
