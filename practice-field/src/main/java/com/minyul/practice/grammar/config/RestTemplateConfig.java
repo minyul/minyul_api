e4f0fa4403557b55299ee5b49efe3f73c6952553
minyul <ggomjae@gmail.com>
2022-03-28 21:36:56 +0900

    feat : 기존 restTemplate 인데.. 음.. webClient 로 갈아타야할텐데

---
+++ practice-field/src/main/java/com/minyul/practice/grammar/config/RestTemplateConfig.java
@@ -0,0 +1,29 @@
+package com.minyul.practice.grammar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+// * 기존 restTemplate
+@Component
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(
+            int connectionTimeout,
+            int readTimeout) {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(this.httpRequestFactory(connectionTimeout, readTimeout));
+
+        return restTemplate;
+    }
+
+    private HttpComponentsClientHttpRequestFactory httpRequestFactory(int connectTimeout, int readTimeout) {
+        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        httpRequestFactory.setReadTimeout(readTimeout);
+        httpRequestFactory.setConnectTimeout(connectTimeout);
+
+        return httpRequestFactory;
+    }
+}
